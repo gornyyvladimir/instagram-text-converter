525f3001ab243cd4a8c702a1b0f15edfd83f549f
Vladimir Gornyj <vladimir.gornyj@piano.io>
2020-07-16 17:18:24 +0300

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,50 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: `Как сделать абзац в инстаграм?`,
+    description: `Не знаете как сделать абзац в инстаграм? Этот онлайн инструмент обязательно вам поможет.`,
+    author: `gornyyvladimir`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/, // See below to configure properly
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+          },
+          {
+            family: `Roboto`,
+            variants: [`300`, `400`],
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Instagram text converter`,
+        short_name: `Instagram text converter`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/assets/icons/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,105 @@
-import React from "react"
+import React, { useState, useRef } from 'react';
+import { convertText } from '../helpers';
+import Notification from '../components/Notification';
+import SEO from "../components/seo"
+import 'normalize.css';
+import './styles.css';
 
-export default function Home() {
-  return <div>Hello world!</div>
+function App() {
+  const textareaRef = useRef(null);
+  const [text, setText] = useState('');
+  const [convertedText, setConvertedText] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [notificationState, setNotificationState] = useState(false);
+
+  const showNotification = (delay = 3000) => {
+    setNotificationState(true);
+    setTimeout(() => {
+      setNotificationState(false);
+    }, delay);
+  };
+
+  const handleTextChange = event => {
+    setText(event.target.value);
+  };
+
+  const handleCovert = event => {
+    event.preventDefault();
+    const convertedText = convertText(text);
+    setConvertedText(convertedText);
+    setDisabled(false);
+  };
+
+  const handleCopy = event => {
+    event.preventDefault();
+    textareaRef.current.select();
+    document.execCommand('copy');
+    showNotification();
+  };
+
+  const handleTextareaFocus = event => {
+    event.target.select();
+  };
+
+  return (
+    <>
+      <SEO />
+      <Notification show={notificationState}>
+        Текст скопирован! Вставьте его в Instagram.
+      </Notification>
+      <div className="container">
+        <h1 className="title">Как сделать абзац в инстаграм?</h1>
+        <p className="text">
+          Этот инструмент поможет вам добавить специальный пробел в инстаграм,
+          чтобы ваши описания в инстаграм могли начинаться с новой строки.
+        </p>
+        <div className="wrapper">
+          <form className="form" onSubmit={handleCovert}>
+            <label className="label" htmlFor="text">
+              Текст, который вы хотите преобразовать
+              <textarea
+                id="text"
+                className="textarea"
+                onChange={handleTextChange}
+                value={text}
+                placeholder="Пожалуйста, введите текст"
+                rows="5"
+                cols="30"
+                required
+              />
+            </label>
+            <button className="button" type="submit">
+              Конвертировать
+            </button>
+          </form>
+          <form className="form" onSubmit={handleCopy}>
+            <label className="label" htmlFor="converted-text">
+              Преобразованный текст
+              <textarea
+                id="converted-text"
+                className="textarea"
+                ref={textareaRef}
+                onFocus={handleTextareaFocus}
+                value={convertedText}
+                placeholder="Здесь будет преобразованный текст"
+                rows="5"
+                cols="30"
+                readOnly
+              />
+            </label>
+            <button className="button" type="submit" disabled={disabled}>
+              Копировать
+            </button>
+          </form>
+        </div>
+        <p className="text text--margin-bottom-2">
+          Количество символов: <b>{text.length}</b>
+          <br />
+          Максимальное количество символов в посте Instagram: <b>2200</b>
+        </p>
+      </div>
+    </>
+  );
 }
+
+export default App;
