db9c011d1c04b248c13702668bff82bf783b574c
gornyyvladimir <gornyyvladimir@gmail.com>
2020-07-20 14:49:49 +0300

    fix notifications

--- src/components/Notification/index.jsx
+++ src/components/Notification/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransitionGroup } from 'react-transition-group';
 import SuccessIcon from '../../assets/icons/success.svg';
 import './styles.css';
 
 const Notification = ({ show, children }) => (
-  <div className={`notification ${show ? '' : 'hide'}`}>
-    <div className="icon">
-      <SuccessIcon width="32px" height="32px" fill="#fff" />
-    </div>
-    <p className="text no-margin">{children}</p>
-  </div>
+  <CSSTransitionGroup
+    transitionName="notification"
+    transitionEnterTimeout={300}
+    transitionLeaveTimeout={300}
+  >
+    {show && (
+      <div className="notification">
+        <div className="icon">
+          <SuccessIcon width="32px" height="32px" fill="#fff" />
+        </div>
+        <p className="text no-margin">{children}</p>
+      </div>
+    )}
+  </CSSTransitionGroup>
 );
 
 Notification.propTypes = {
